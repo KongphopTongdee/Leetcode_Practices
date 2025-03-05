707decfb31401298a3e43b41a86fb52f131fb61a
KongphopTongdee <kongphop.working@gmail.com>
2025-03-05 21:23:55 +0700

    Add new question 461_HammingDistance

---
+++ EasyCode/461_HammingDistance.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        pass
+    
+    
+checkAns = Solution()
+checkAns.hammingDistance( 1, 4 )
+checkAns.hammingDistance( 3, 1 )
