0bfa4978dc3476c34368391b9b08e8a542b2976d
KongphopTongdee <119693093+KongphopTongdee@users.noreply.github.com>
2025-03-18 15:43:14 +0700

    Update 190_ReverseBits.py
    Test in Leetcode isn't work but test in complier is work.

--- EasyCode/190_ReverseBits.py
+++ EasyCode/190_ReverseBits.py
@@ -1,30 +1,30 @@
 class Solution:
     def __init__( self ):
         self.answer = 0
+        
+    def reveseBit( self, value ):
+        return value[::-1]
     
     def convertToDecimal( self, value ):
-        reverseStr = value[::-1]
         sumation = 0
-        newValue = 0
-        print( reverseStr )
-    
         
-        for idx in range( 0, len( reverseStr ) - 1 ):
-            newValue = 2**( ( len( reverseStr ) - 1 ) - idx )
-            print( newValue )
-            if( reverseStr[ ( len( reverseStr ) - 1 ) - idx ] == "1" ):
-                sumation += 2**( ( len( reverseStr ) - 1 ) - idx )
-            
-        print( sumation )
+        for idx in range( 0, len( value ) ):
+            if( value[ idx ] == "1" ):
+                sumation += 2**( len( value ) - 1 - idx )
+                
+        return sumation
     
     def reverseBits( self, n ):
-        self.convertToDecimal( n )
-        
+        n = str( n )
+        reverseInput = self.reveseBit( n )
+        self.answer = self.convertToDecimal( reverseInput )
+        print( self.answer )
+        return self.answer
 
 
 checkAns = Solution()
-# checkAns.reverseBits( "00000010100101000001111010011100" )                # Output: 964176192 
-# checkAns.reverseBits( "11111111111111111111111111111101" )                # Output: 3221225471
-checkAns.reverseBits( "1101" )                # Output: 3221225471
+checkAns.reverseBits( "00000010100101000001111010011100" )                # Output: 964176192 
+checkAns.reverseBits( "11111111111111111111111111111101" )                # Output: 3221225471
+# checkAns.reverseBits( "1101" )                                            # Output: 11
 
         
