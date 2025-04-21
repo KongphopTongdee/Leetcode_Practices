58d82b49b59374b9d5c5b7598c2164c207ac4c25
KongphopTongdee <kongphop.working@gmail.com>
2025-04-21 22:45:12 +0700

    463_IslandPerimeterCreateFunctionSearch

--- EasyCode/463_IslandPerimeter.py
+++ EasyCode/463_IslandPerimeter.py
@@ -2,18 +2,46 @@ class Solution:
     def __init__( self ):
         self.answer = 0
     
-    def findAllIsland( self, girdInput ):
-        girdCount = 0
+    def positionAllIsland( self, girdInput ):
+        allPosition = []
+        islandPosition = []
         
-        for i in range( len( girdInput ) ):
-            for j in range( len( girdInput[ i ] ) ):
-                if( girdInput[ i ][ j ] == 1 ):
-                    girdCount += 1
+        for xIndex in range( len( girdInput ) ):
+            for yIndex in range( len( girdInput[ xIndex ] ) ):
+                allPosition.append( ( xIndex, yIndex ) )
+                if( girdInput[ xIndex ][ yIndex ] == 1 ):
+                    islandPosition.append( ( xIndex, yIndex ) )
         
+        # print( "allPosition: ", allPosition )
+        # print( "islandPosition: ", islandPosition )
         
+        return islandPosition, allPosition
+    
+    def countBorderLine( self, areaIsland, allIsland ):
+        countBorderLine = 0
+        for searchBorderLine in areaIsland:
+            xIndex, yIndex = searchBorderLine
+            topArea = ( xIndex, yIndex - 1 )
+            bottomArea = ( xIndex, yIndex + 1 )
+            leftArea = ( xIndex - 1, yIndex )
+            rightArea = ( xIndex + 1, yIndex )
+            
+            if( ( topArea in areaIsland ) and ( bottomArea in areaIsland ) and ( leftArea in areaIsland ) and ( rightArea in areaIsland ) ):
+                countBorderLine += 0
+            
+            # need to check 3 axis, 2 axis, 1 axis    
+                
+                
+                
+        return countBorderLine
     
     def islandPerimeter( self, grid ):
-        self.findAllIsland( grid )
+        areaIsland, allIsland = self.positionAllIsland( grid )
+        borderLine = self.countBorderLine( areaIsland, allIsland )
+        
+        self.answer = borderLine
+        
+        return self.answer
         
     
 checkAns = Solution()
