6bdca6b633ef944abc3b0088f3b14e396f79d5a4
KongphopTongdee <119693093+KongphopTongdee@users.noreply.github.com>
2025-04-24 15:20:55 +0700

    Update 118_Pascal'sTriangle24042025.py

--- EasyCode/118_Pascal'sTriangle.py
+++ EasyCode/118_Pascal'sTriangle.py
@@ -1,3 +1,27 @@
 class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
+    def __init__( self ):
+        self.answer = []
+        self.STARTARR = [ 1, 1 ]
         
+    def createTriangle( self, rowsInput ):
+        allPascal = [ [ 1, 1 ] ]
+        curentArr = [ 1, 1 ]
+        if( rowsInput >= 3 ):
+            for idx in range( rowsInput - 1 ):
+                
+            
+        return allPascal
+    
+    def generate( self, numRows ):
+        if( numRows == 1 ):
+            self.answer.append( [ 1 ] )
+        elif( numRows > 1 ):
+            pascalArr = self.createTriangle( numRows )
+            self.answer = pascalArr
+            
+        return self.answer
+
+
+checkAns = Solution()
+print( checkAns.generate( 5 ) )
+# print( checkAns.generate( 1 ) )
