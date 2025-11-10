e81073be08abfaca37024de06c4ad63cc9fc437e
KongphopTongdee <119693093+KongphopTongdee@users.noreply.github.com>
2025-11-10 13:28:54 +0700

    Create 79_WordSearch.py
    Create init of this question

---
+++ MediumCode/79_WordSearch.py
@@ -0,0 +1,19 @@
+class Solution:
+    def __init__( self ):
+        self.answer = None
+        
+    def exist( self, board, word ):
+        pass
+    
+    def displayOutput( self, ):
+        pass
+    
+    def test( self ):
+        pass
+    
+checkAns = Solution()
+checkAns = displayOutput()
+checkAns = test()
+checkAns.exist( board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED" )
+checkAns.exist( board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE" )
+checkAns.exist( board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB" )
